bbe728c73a7748ba0ea2703baaba827fb5933ae7
Ada Rose Edwards <ada@ada.is>
2017-09-22 15:40:05 +0100

    make it a web service

--- index.js
+++ index.js
@@ -1,41 +1,19 @@
-const { writeFileSync } = require('fs');
-const { promisify } = require('util');
-const { Builder, By, Key, promise, until } = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-promise.USE_PROMISE_MANAGER = false;
+const express = require('express');
+const snap = require('./snap');
 
-const binary = new firefox.Binary('/home/SERILOCAL/ada.edwards/bin/firefox/firefox');
-// binary.addArguments("--headless");
+const app = express();
 
-const profile = new firefox.Profile();
-profile.setPreference("browser.download.dir", __dirname);
-profile.setPreference("browser.download.folderList", 2);
-profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/png;"); 
-profile.setPreference("browser.download.manager.showWhenStarting", false );
+app.get('/', function (req, res) {
 
-const options = new firefox.Options();
-options.setBinary(binary);
-options.setProfile(profile);
-
-const driver = new Builder()
-.forBrowser('firefox')
-.setFirefoxOptions(options)
-.build();
-
-async function snap(url) {
-	await driver.get(url);
-
-	await driver.wait(async () => {
-		const readyState = await driver.executeScript('return document.querySelector("a-scene").hasLoaded');
-		return readyState === true;
+	
+	const url = req.query.url;
+	if (!url) return res.sendFile(__dirname + '/index.html');
+	
+	snap(url).then(function (filepath) {
+		res.sendFile(filepath);
+	}).catch(e => {
+		res.end(e.message);	
 	});
+});
 
-	await driver.executeScript('document.querySelector("a-scene").components.screenshot.capture("equirectangular")');
-
-	await new Promise(resolve => setTimeout(resolve, 2000));
-
-	await driver.quit();
-}
-
-snap('https://earthy-course.glitch.me/');
-  
+app.listen(process.env.PORT || 3000);
---
+++ snap.js
@@ -0,0 +1,54 @@
+const { writeFileSync } = require('fs');
+const { promisify } = require('util');
+const { Builder, By, Key, promise, until } = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+const fs = require('fs');
+const snapPath = __dirname + '/snaps/';
+promise.USE_PROMISE_MANAGER = false;
+
+const binary = new firefox.Binary('/home/SERILOCAL/ada.edwards/bin/firefox/firefox');
+// binary.addArguments("--headless");
+
+const profile = new firefox.Profile();
+profile.setPreference("browser.download.dir", snapPath);
+profile.setPreference("browser.download.folderList", 2);
+profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/png;"); 
+profile.setPreference("browser.download.manager.showWhenStarting", false );
+
+const options = new firefox.Options();
+options.setBinary(binary);
+options.setProfile(profile);
+
+async function snap(url) {
+	
+	const driver = new Builder()
+	.forBrowser('firefox')
+	.setFirefoxOptions(options)
+	.build();
+
+	await driver.get(url);
+
+	await driver.wait(async () => {
+		const readyState = await driver.executeScript('return document.querySelector("a-scene").hasLoaded');
+		return readyState === true;
+	});
+
+	await new Promise(resolve => setTimeout(resolve, 3000));
+
+	const files = fs.readdirSync(snapPath);
+	
+	await driver.executeScript('document.querySelector("a-scene").components.screenshot.capture("equirectangular")');
+	
+	let newFiles;
+
+	await driver.wait(() => {
+		newFiles = fs.readdirSync(snapPath);
+		return newFiles.length > files.length;
+	});
+
+	await driver.quit();
+
+	return snapPath + newFiles.find(file => files.indexOf(file) === -1);
+}
+
+module.exports = snap;
