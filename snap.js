525e93e84609bd4d474ae9b584facad6399e3fe5
Ada Rose Edwards <ada@ada.is>
2017-09-22 15:50:06 +0100

    ch-ch-changes

--- snap.js
+++ snap.js
@@ -3,23 +3,24 @@ const { promisify } = require('util');
 const { Builder, By, Key, promise, until } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const fs = require('fs');
-const snapPath = __dirname + '/snaps/';
 promise.USE_PROMISE_MANAGER = false;
 
 const binary = new firefox.Binary('/home/SERILOCAL/ada.edwards/bin/firefox/firefox');
 // binary.addArguments("--headless");
 
-const profile = new firefox.Profile();
-profile.setPreference("browser.download.dir", snapPath);
-profile.setPreference("browser.download.folderList", 2);
-profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/png;"); 
-profile.setPreference("browser.download.manager.showWhenStarting", false );
-
-const options = new firefox.Options();
-options.setBinary(binary);
-options.setProfile(profile);
-
 async function snap(url) {
+	const snapPath = __dirname + '/snaps/' + Date.now() + '/';
+	fs.mkdirSync(snapPath);
+	
+	const profile = new firefox.Profile();
+	profile.setPreference("browser.download.dir", snapPath);
+	profile.setPreference("browser.download.folderList", 2);
+	profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/png;"); 
+	profile.setPreference("browser.download.manager.showWhenStarting", false );
+	
+	const options = new firefox.Options();
+	options.setBinary(binary);
+	options.setProfile(profile);
 	
 	const driver = new Builder()
 	.forBrowser('firefox')
